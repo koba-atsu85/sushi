baee58ee34901efe32e183b7ce50c198a05f0992
kobayashi <atsushi.kobayashi@ap-com.co.jp>
2017-03-15 18:17:10 +0900

    add User model

---
+++ sushi/db/migrate/20170315091147_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.integer :id
+      t.string :name01
+      t.string :name02
+      t.string :mail
+      t.string :password
+      t.timestamp :create_date
+      t.timestamp :update_date
+      t.integer :del_flg
+
+      t.timestamps
+    end
+  end
+end
